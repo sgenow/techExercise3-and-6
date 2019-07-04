b32405ebda730771cf2621dcdf3e949d57a3db48
sgenow <sgenow@omahamarian.org>
2019-07-04 12:11:04 -0500

    Experimenting with unit testing using JUnit

---
+++ src/main/java/com/bhaiti/kela/beans/StudentRegistrationReplyTest.java
@@ -0,0 +1,29 @@
+package com.bhaiti.kela.beans;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StudentRegistrationReplyTest {
+
+	@Test
+	public void testSetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetAge() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotify() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFinalize() {
+		fail("Not yet implemented");
+	}
+
+}
